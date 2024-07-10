c23f72ebeec01f175d483930c0b02be23489c788
Preetham John <78245284+pj323@users.noreply.github.com>
2024-07-10 11:42:29 -0700

    Update autocomplete.sh

--- autocomplete.sh
+++ autocomplete.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+
+# Function to perform autocomplete from command history
+_autocomplete_from_history() {
+    local cur prev opts
+    COMPREPLY=()   # Array variable storing the possible completions
+    cur="${COMP_WORDS[COMP_CWORD]}"  # Current word being completed
+    prev="${COMP_WORDS[COMP_CWORD-1]}"  # Previous word
+
+    # Generate command options from history
+    opts=$(history | cut -c 8- | awk '{$1=$1};1' | grep "^$cur" | sort -u)
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+}
+
+# Register the function to provide completions for the first word of the command line.
+complete -F _autocomplete_from_history
 
